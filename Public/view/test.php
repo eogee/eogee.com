fd0db941ac7997c5bda17cac9535145f90439a2a
eogee <eogee@qq.com>
2025-01-19 01:00:50 +0800

    feat(core): 添加编辑器预览功能

--- Public/view/test.php
+++ Public/view/test.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="/layui/css/layui.css">
     <!-- 引入编辑器的css文件 -->
     <link rel="stylesheet" href="/dist/editor.md/css/editormd.min.css" />
+    <link rel="stylesheet" href="/dist/editor.md/css/editormd.preview.css" />
     <script src="/layui/layui.js"></script>
     <script src="/js/admin/eogee-admin-layui.js"></script>
 </head>
@@ -15,27 +16,41 @@
     <div class="layui-container">
         <br><br>
         <div id="test-editormd">
-            <textarea style="display:none;"></textarea>
+            <textarea name="content" style="display:none;"></textarea>
         </div>
+        <div id="preview"></div>
         <br><br>
     </div>
     
     <script src = "/dist/jquery/dist/jquery.min.js"></script>
+    <script src = "/dist/editor.md/lib/marked.min.js"></script>
+    <script src = "/dist/editor.md/lib/prettify.min.js"></script>
+    <script src = "/dist/editor.md/lib/raphael.min.js"></script>
+    <script src = "/dist/editor.md/lib/underscore.min.js"></script>
+    <script src = "/dist/editor.md/lib/flowchart.min.js"></script>
+    <script src = "/dist/editor.md/lib/sequence-diagram.min.js"></script>
+    <script src = "/dist/editor.md/lib/jquery.flowchart.min.js"></script>
     <script src = "/dist/editor.md/editormd.min.js"></script>
     <script>
-        // 使用editor.md编辑器
-        var testEditor = editormd("test-editormd", {
-            width   : "100%",
-            height  : 640,
-            syncScrolling : "single",
-            path    : "/dist/editor.md/lib/",
-            imageUpload : true,
-            imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
-            imageUploadURL : "/test/fileUploadApi",
-            /* onload : function() {
-                this.fullscreen();
-            } */
+        $(document).ready(function() {
+            var editor = editormd("test-editormd", {
+                width: "100%",
+                height: "600px",
+                path: "/dist/editor.md/lib/", // editormd的lib目录路径
+                // 其他配置项
+                imageUpload: true,
+                imageUploadURL: "/test/fileUploadApi", // 上传图片的URL
+                crossDomainUpload: false,
+                uploadCallbackURL: "callback.php"
+            });
+
+            var editor = editormd.markdownToHTML("preview", {
+                markdown: "Hello world!", // Markdown内容
+                path: "/dist/editor.md/lib/", // editormd的lib目录路径
+            });
         });
+
+        
     </script>
 </body>
 </html>
