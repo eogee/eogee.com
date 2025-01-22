f7f8137a3700fae37e96a658fd0deb267f5be705
eogee <eogee@qq.com>
2025-01-23 05:34:18 +0800

    refactor: 合并div类名以简化结构

--- Public/view/index/detailChild.php
+++ Public/view/index/detailChild.php
@@ -6,15 +6,14 @@
     $data = $data['data'];
 ?> 
     
-    <div class="eog-container eog-index-main">
-        <div class = "eog-image-background-half" style = "background-image:url('<?=$data['data']['backgroundImage']?>')">
-            <div class="layui-container  eog-text">
+    <div class="eog-container">
+        <div class = "eog-image-background-half eog-index-main" style = "background-image:url('<?=$data['data']['backgroundImage']?>')">
+            <div class="layui-container eog-text">
                 <h1 class = "<?= $data['data']['top'] ? 'padding-top-60' : null ?> layui-font-<?=$data['data']['titleColor']?> eog-text-shadow-<?=$data['data']['titleShadowColor']?>"><?=$data['data']['title']?></h1>
                 <h3 class = "layui-font-<?=$data['data']['keynoteColor']?> eog-text-shadow-<?=$data['data']['keynoteShadowColor']?>"><?=$data['data']['keynote']?></h3>
             </div>
         </div>
-        <div class="layui-container  eog-text">
-            <p class = "layui-font-<?=$data['data']['contentColor']?> eog-text-shadow-<?=$data['data']['contentShadowColor']?>"><?=$data['data']['content']?></p>
+        <div class="layui-container eog-text">
             <p class = "layui-font-<?=$data['data']['detailColor']?> eog-text-shadow-<?=$data['data']['detailShadowColor']?>"><?=$data['data']['detail']?></p>
         </div>
     </div>
